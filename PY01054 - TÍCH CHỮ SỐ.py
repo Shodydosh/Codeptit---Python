b58e99b66542eeb81b92de3535306207024c39ec
Tung Nguyen Minh <87895460+Shodydosh@users.noreply.github.com>
2023-08-26 14:54:40 +0700

    Create PY01054 - TÍCH CHỮ SỐ.py

---
+++ PY01054 - TÍCH CHỮ SỐ.py
@@ -0,0 +1,32 @@
+import math
+
+def isPrime(x):
+    a = int(x)
+    if(a<=1): return False
+    if(a<=3): return True
+    if(a % 2 == 0 or a % 3 == 0): return False
+    sqrt_num = int(math.sqrt(a))
+    for i in range(4, sqrt_num +1, 6):
+        if a % i == 0 or a % (i+ 2) == 0:
+            return False
+    return True 
+
+def solve(a):
+    chars = [char for char in a]
+    ans = 1
+    for char in chars:
+        if char != '0':
+            ans *= int(char)
+    return ans
+            
+def main():
+    t = int(input())
+    while t > 0:
+        t -= 1
+        a = str(input())
+        print(solve(a))
+        # if(solve(a) == True): print("YES")
+        # else: print("NO")
+
+if __name__ == "__main__":
+    main()
