adc84d19de1b14986a4ca0b3d10bd1eb6c8f99e9
Tung Nguyen Minh <87895460+Shodydosh@users.noreply.github.com>
2023-08-26 16:21:25 +0700

    Create PY01059 - TỔNG CHỮ SỐ - TÍCH CHỮ SỐ.py

---
+++ PY01059 - TỔNG CHỮ SỐ - TÍCH CHỮ SỐ.py
@@ -0,0 +1,39 @@
+import math
+
+def isPrime(a):
+    if(a<=1): return False
+    if(a<=3): return True
+    if(a % 2 == 0 or a % 3 == 0): return False
+    sqrt_num = int(math.sqrt(a))
+    for i in range(4, sqrt_num +1, 6):
+        if a % i == 0 or a % (i+ 2) == 0:
+            return False
+    return True 
+
+def solve(s):
+    x = 0
+    y = 1
+    chk = True
+    for i in range(0, len(s)):
+        if i % 2 == 0:
+            x += int(s[i])
+        else:
+            if(s[i] != '0'):
+                chk = False
+                y *= int(s[i])
+    if chk: y = 0
+    print( x, y)
+
+            
+def main():
+    t = int(input())
+    while t > 0:
+        t -= 1
+        a = str(input())
+        solve(a)
+        # print(solve(a))
+        # if solve(a): print("YES")
+        # else: print("NO")
+
+if __name__ == "__main__":
+    main()
