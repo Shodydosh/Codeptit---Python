1eb5e221b0c5e3d9653be25326a911533d2ad871
Tung Nguyen Minh <87895460+Shodydosh@users.noreply.github.com>
2023-09-07 22:07:38 +0700

    Create PY01064 - KÝ TỰ THỨ K.py

---
+++ PY01064 - KÝ TỰ THỨ K.py
@@ -0,0 +1,10 @@
+import math
+
+def solve(n, k):
+    if k == 2 ** (n-1): return n
+    if k > 2 ** (n-1): return solve(n-1, k-2**(n-1))
+    else: return solve(n-1, k)
+
+for _ in range(int(input())):
+    n, k = list(map(int, input().split()))
+    print(chr(solve(n, k) + ord('A') - 1))
