e0642bb7bb2a221a9cc1320daaf04c782e443852
Tung Nguyen Minh <87895460+Shodydosh@users.noreply.github.com>
2023-09-15 11:05:34 +0700

    Create PY03004 - THỐNG KÊ TỪ KHÁC NHAU.py

---
+++ PY03004 - THỐNG KÊ TỪ KHÁC NHAU.py
@@ -0,0 +1,17 @@
+import re
+words = []
+for _ in range(int(input())):
+    text = input()
+    words += re.findall(r'\b\w+\b', text.lower())
+dict = {}
+for w in words:
+    if w not in dict:
+        dict[w] = 1
+    else: dict[w] += 1
+
+freq = []
+for key, value in dict.items():
+    freq.append({"w" : key, "f": value})
+freq = sorted(freq, key = lambda x : (-x["f"], x["w"]))
+for a in freq:
+    print(a["w"], a["f"])
